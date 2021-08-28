3326591eca8867a0f1c98adf26fd5813ec439df7
罗超 <2422635277@qq.com>
2021-08-28 23:18:06 +0800

    1

---
+++ leetcode/19. 删除链表的倒数第 N 个结点/1.js
@@ -0,0 +1,18 @@
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+ var removeNthFromEnd = function(head, n) {
+    let ret = new ListNode(0, head),
+        slow = fast = ret;
+    while(n--) fast = fast.next;
+    if(!fast) return ret.next;
+    while (fast.next) {
+        fast = fast.next; 
+        slow = slow.next
+    };
+    slow.next = slow.next.next;
+    return ret.next;
+};
+
