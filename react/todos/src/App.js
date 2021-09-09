bbb3659cfd6f36bd0fe2b10e3d0fd02c41cf2a5a
罗超 <2422635277@qq.com>
2021-09-09 22:37:12 +0800

    1

--- react/todos/src/App.js
+++ react/todos/src/App.js
@@ -1,25 +1,52 @@
-import {useState} from 'react';
-import logo from './logo.svg';
-import ChartDemo from './components/ChartDemo';
-import './App.css';
+import { useState } from 'react';// 引入useState 定义以及修改数据的方法
+import './App.css' // 引入样式
 
-const Count = () => {
-  let [count, setCount] = useState(0)
+const App = () => {
+  const [todos, setTodos] = useState([
+    {
+      content: 'Pickup dry cleaning',
+      isCompleted: true
+    },
+    {
+      content: 'Get haircut',
+      isCompleted: false
+    },
+    {
+      content: 'Build a todo app in React',
+      isCompleted: false
+    }
+  ]) 
+  const toggleTodoCompleteAtIndex = (i) => {
+    const newTodos = [...todos];
+    // todos 某一条 ！
+    newTodos[i].isCompleted = !newTodos[i].isCompleted
+    console.log(todos[i]);
+    setTodos(
+      newTodos
+    )
+  }
   return (
-    <div>
-      <p>You clicked {count} times</p>
-      <button onClick={() =>setCount(count+1)}>click me</button>
+    <div className="app">
+      <form  className="todo-list">
+        <ul>
+          {
+            todos.map((todo, i) => (
+              <div className={`todo ${todo.isCompleted&&'todo-is-completed'}`}>
+                <div className="checkbox" onClick={() => toggleTodoCompleteAtIndex(i)}>
+                {
+                  todo.isCompleted && (
+                    <span>&#x2714;</span>
+                  )
+                }
+                </div>
+                <input type="text" value={todo.content}/>
+              </div>
+            ))
+          }
+          
+        </ul>
+      </form>
     </div>
   )
 }
-
-function App() {
-  return (
-    <div className="App">
-      {/* <Count/> */}
-      <ChartDemo/>
-    </div>
-  );
-}
-
-export default App;
+export default App
--- react/todos/src/components/ChartDemo.js
+++ react/todos/src/components/ChartDemo.js
@@ -1,20 +1,45 @@
-import {useRef, useEffevt, useEffect} from 'react'
-import * as eCharts from 'echarts';
-const ChartDemo = () => {
-    let eChartsRef = useRef() //唯一的元素
-    useEffect(() => {
-        console.log(eChartsRef.current,'...........');
-        const myChart = eCharts.init(eChartsRef.current);
-    },[]) //onload
-    return (
-        // dom 不要 ref
-        <div ref={eChartsRef} style={{
-            width: 600,
-            height: 400,
-            margin: 100,
-        }}>
+import { useRef, useEffect } from 'react';
+import  * as eCharts from 'echarts';
 
-        </div>
-    )
+const ChartDemo = () => {
+  let eChartsRef = useRef() // 唯一的元素
+  console.log(eChartsRef, '---------------|||||')
+  useEffect(() => {
+    console.log(eChartsRef.current, '-------------------------------')
+    
+    const myChart = eCharts.init(eChartsRef.current);
+    let option = {
+      title: {
+        text: 'ECahrts 入门实战'
+      },
+      tooltip: {},
+      legend: {
+        data: ["销量"]
+      },
+      xAxis: {
+        data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+      },
+      yAxis: {},
+      series: [
+        {
+          name: "销量",
+          type: "bar",
+          data: [5, 20, 36, 10, 10, 20]
+        }
+      ]
+    }
+    myChart.setOption(option)
+
+  }, []) // onload 
+  return (
+    // dom 不要 ref 
+    <div ref={eChartsRef} style={{
+      width: 600,
+      height: 400,
+      margin: 100
+    }}>
+      
+    </div>
+  )
 }
 export default ChartDemo
