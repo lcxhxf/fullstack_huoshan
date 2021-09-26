4136eb26c0e7633693f84aee95b24f27ad6d28e6
罗超 <2422635277@qq.com>
2021-09-26 22:54:17 +0800

    leetcode

---
+++ leetcode/23. 合并K个升序链表/1.js
@@ -0,0 +1,20 @@
+var mergeKLists = function (lists) {
+    const list = [];
+    for (let i = 0; i < lists.length; i++) {
+        let node = lists[i];
+        while (node) {
+            list.push(node.val);
+            node = node.next;
+        }
+    }
+    list.sort((a, b) => a - b);
+    const res = new ListNode();
+    let now = res;
+    // console.log(list)
+    for (let i = 0; i < list.length; i++) {
+        now.next = new ListNode(list[i]);
+        now = now.next;
+    }
+    return res.next;
+};
+
